eb99886917c291046d2a51bb0bba6ed5e31544a7
Silvia Donaire <silviadonaireserrano@gmail.com>
2024-03-03 20:38:19 +0100

    add files

--- django-recofood/asgi.py
+++ django-recofood/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend_orienta_tech project.
+ASGI config for django-recofood project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_orienta_tech.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-recofood.settings')
 
 application = get_asgi_application()
